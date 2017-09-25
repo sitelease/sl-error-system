ca827d36f0abdeeda4480f107ff815556ae20da2
Benjamin Blake <ben@sitelease.ca>
2017-09-24 19:30:44 -0600

    First Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Sl-error-system #
+
+### What is this repository for? ###
+This package contains error handling/notifying helper classes for sitelease's silverstripe modules
+
+### How do I get set up? ###
+**Step 1**: Install a localhost server (we personally use ampps but wampp,mampp,xampp or any other localhost server will do.) and then start apache and mysql.
+
+**Step 2**: Install Composer via the commandline (linux,mac) or via the installer (windows)
+
+**Step 3**: Install Silverstripe through composer with this command in the terminal (linux,mac) or cmd (windows)
+`composer create-project silverstripe/installer ./silverstripe`
+
+**Step 4**:Travel to `http://localhost/silverstripe` in your browser and go through the setup.
+
+**Step 5**:Download the repo and unzip the archive in the base of the silverstripe site.
+
+**Step 6**: Now login to silverstripe and travel to `http://localhost/silverstripe/dev/build?flush` to build the database and flush the cache.
+
+**Step 7**: You will now be able to use the module's features
---
+++ code/SLErrorSystem.php
@@ -0,0 +1,135 @@
+<?php
+
+/**
+ * This class is the main error class for sitelease's error system.
+ *
+ *
+ * @author Benjamin Blake (sitelease.ca)
+ * @package sl-error-system
+ */
+class SLErrorSystem extends ViewableData
+{
+    /**
+    * Used to construct the server error page url.
+    *
+    * @author Benjamin Blake (sitelease.ca)
+    * @var string
+    * @config
+    */
+    private static $errorPageURLSegment = "server-error/";
+
+    /**
+    * The Email address you would like notified when an error occurs on the site
+    *
+    * @author Benjamin Blake (sitelease.ca)
+    * @var string
+    * @config
+    */
+    private static $mailTo = "Administrator <error_notification@sitelease.ca>";
+
+    /**
+     * Used to create the subject that will be displayed on error email notifications for this site.
+     *
+     * NOTE: The subject format is $subjectPrefix . $siteurl . $subjectSuffix
+     *
+     * @author Benjamin Blake (sitelease.ca)
+     * @var string
+     * @config
+     */
+    private static $mailSubjectPrefix = "Sitelease Error Notification for ";
+
+    /**
+     * Used to create the subject that will be displayed on error email notifications for this site
+     *
+     * NOTE: The subject format is $subjectPrefix . $siteurl . $subjectSuffix
+     *
+     * @author Benjamin Blake (sitelease.ca)
+     * @var string
+     * @config
+     */
+    private static $mailSubjectSuffix = "";
+
+
+    /**
+     * A custom error handler that converts all php errors into exceptions that
+     * can be caught using a try/catch.
+     * This is useful (and in most cases needed) for database transactions, as
+     * you do not want any errors that are not considered exceptions to
+     * cause data mismatching in your database.
+     *
+     * NOTE: You need to make sure to reset the error handler back to
+     * silverstripes when you are finished. (Use restore_error_handler();)
+     *
+     * @author Benjamin Blake (sitelease.ca)
+     * @param  [type] $errno      [description]
+     * @param  [type] $errstr     [description]
+     * @param  [type] $errfile    [description]
+     * @param  [type] $errline    [description]
+     * @param  array  $errcontext [description]
+     * @return [type]             [description]
+     */
+    public static function errorsAsExceptionsErrorHandler($errno, $errstr, $errfile, $errline, array $errcontext) {
+        // error was suppressed with the @-operator
+        if (0 === error_reporting()) {
+            return false;
+        }
+
+        throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+    }
+
+    /**
+     * Logs an error to the system logger and then notifies an admin via email
+     *
+     * @author Benjamin Blake (sitelease.ca)
+     * @param  string $classAndMethodName The name of the file and method that triggered the error. Use __METHOD__.
+     * @param  string $message An extra mesage that will be presented with the official message. Useful for describing what happened.
+     * @param  string $error The official error or exception message (optional)
+     * @return boolean Returns true if mail successfully accepted for delivery. Otherwise returns false
+     */
+    public static function sendErrorNotification($classAndMethodName, $message, $error = null)
+    {
+        // Build message from parameters
+        $mailMessage = $classAndMethodName;
+        $mailMessage .= " - " . $message;
+        if(!empty($error)){
+            $mailMessage .= "<br  />Error: " . $error;
+        }
+        // Log the error to the system's error log
+        error_log($mailMessage);
+        // Notify an admin via email
+        $mailTo = self::config()->mailTo;
+        $mailSubject = self::config()->mailSubjectPrefix . Director::absoluteBaseURL() . self::config()->mailSubjectSuffix;
+        // Try to deliver the email
+        $delivery = mail($mailTo, $mailSubject, $mailMessage);
+        // Return true if mail successfully accepted for delivery
+        if($delivery){
+            return true;
+        }else{
+            // Otherwise returns false
+            return false;
+        }
+    }
+
+    /**
+     * Logs and error and notifies an admin using self::sendErrorNotification()
+     * Then redirects to the 500 server error page
+     *
+     * @param  [type] $controller         The controller that will be used for the redirect
+     * @param  string $classAndMethodName The name of the file and method that triggered the error. Use __METHOD__.
+     * @param  string $message An extra mesage that will be presented with the official message. Useful for describing what happened.
+     * @param  string $error The official error or exception message (optional)
+     * @return [type] Returns a redirect to the server error page
+     */
+    public static function throw500Error($controller, $classAndMethodName, $message, $error = null)
+    {
+        $absoluteSiteURL = Director::absoluteBaseURL();
+        if(empty($devMessage)){
+            $devMessage = "A 500 error occured on " . $absoluteSiteURL;
+        }
+        self::sendErrorNotification($classAndMethodName, $message, $error);
+        return $controller->redirect(Controller::join_links(
+            $absoluteSiteURL,
+            self::config()->errorPageURLSegment
+        ));
+    }
+}
